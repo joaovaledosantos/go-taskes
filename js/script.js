5783818fde29a30adedfc5547afcba5d66acc95f
João V. Santos <joaovaledosantos@gmail.com>
2024-02-13 03:00:06 -0300

    [Aula-126] Salvando tarefa no LocalStorage

--- js/script.js
+++ js/script.js
@@ -1,20 +1,39 @@
 const table = document.getElementById('table-body');
 const modal = document.getElementById('modal');
 const form = document.getElementById('form');
-const inputDescription = document.getElementById('description');
+const inputDescription = document.getElementById('Description');
 const inputDate = document.getElementById('date');
 const loadingMessage = document.getElementById('loading-message');
 const countTasks = document.getElementById('count-tasks');
 const btnCreateTask = document.getElementById('btn-create-task');
 
 
-function openModal() {
-    modal.style.display = 'flex';
+//MODAL ===================================
+btnCreateTask.addEventListener('click', CreateTask);
+
+function CreateTask(e) {
+    e.preventDefault();
+
+    if (!inputDescription.value || !inputDate.value) {
+        alert('Preencha todos os campos!');
+        return;
+    }
+
+
+      const newTask = {
+          description: inputDescription.value,
+          Date: new Date(inputDate.value).toLocaleDateString(),
+          id: Math.floor(Math.random() * 10000)
+     }
+
+    localStorage.setItem('@GoTasks', JSON.stringify([ newTask ]));
+
+    toggleModal();
+    clearFields();
 }
 
-function closeModal() {
-    modal.style.display = 'none';
-    clearFields();
+function toggleModal() {
+    modal.classList.toggle('modal-visible');
 }
 
 function clearFields() {
