ace73a1165d9dcd4ced1079ca0dfc2c9a60b5d43
João V. Santos <joaovaledosantos@gmail.com>
2024-02-13 23:14:48 -0300

    [Aula-129]  Adicionando tarefas na tabela

--- js/index.js
+++ js/index.js
@@ -2,14 +2,30 @@ const table = document.getElementById('table-body');
 const loadingMessage = document.getElementById('loading-message');
 const countTasks = document.getElementById('count-tasks');
 
-
-function loadTasks(){
-    const tasks =  getTasks() || [];
-    return tasks;
-}
-
 function updateCountTasks() {
-    const allTasks = loadTasks();
+    const allTasks = getTasks();
     countTasks.innerHTML = allTasks.length;
 }
 
+function fillTable() {
+    const allTasks = getTasks();
+
+    if (allTasks.length === 0) {
+        loadingMessage.innerHTML = "Você não tem nenhuma tarefa!";
+    } else {
+        loadingMessage.innerHTML = "";
+    }
+
+     updateCountTasks();
+
+}
+
+function innerHTMLTasks(task) {
+    const html = `
+     <td>$(task.description)</td>
+     <td>$(task.date)</td>
+     <td>Ações</td>
+    `
+
+    return html;
+}
